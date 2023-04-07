9be3a39052dc741c6814061db62fa2919bbecac5
DenkovychOleh <praice07@gmail.com>
2023-04-07 22:00:40 +0300

    add ExecutionServiceFacadeTest

--- src/test/java/executor/service/services/ExecutionServiceFacadeTest.java
+++ src/test/java/executor/service/services/ExecutionServiceFacadeTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import static org.mockito.Mockito.*;
-
 class ExecutionServiceFacadeTest {
 
     private ExecutionServiceFacade executionServiceFacade;
