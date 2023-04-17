47696af32a6ebc28ab74ff53d738dabcea50d95a
Oleh Denkovych <macbook@MacBooks-MacBook-Pro.local>
2023-04-17 23:31:22 +0300

    Migrate service ExecutionService to the Spring.

--- src/main/java/executor/service/services/ExecutionServiceFacade.java
+++ src/main/java/executor/service/services/ExecutionServiceFacade.java
@@ -3,12 +3,15 @@ package executor.service.services;
 import executor.service.model.ProxyConfigHolder;
 import executor.service.model.Scenario;
 import org.openqa.selenium.WebDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class ExecutionServiceFacade implements ExecutionService {
     private final ScenarioExecutorImpl scenarioExecutor;
     private final ScenarioSourceListenerImpl scenarioSourceListener;
     private final WebDriverInitializer webDriverInitializer;
-
+@Autowired
     public ExecutionServiceFacade(ScenarioExecutorImpl scenarioExecutor, ScenarioSourceListenerImpl scenarioSourceListener, WebDriverInitializer webDriverInitializer) {
         this.scenarioExecutor = scenarioExecutor;
         this.scenarioSourceListener = scenarioSourceListener;
